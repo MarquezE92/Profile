2c4fbc738f7a01066c2bb3e47272bfb7a1e61f82
MarquezE92 <vsuykyasmbp@hotmail.com>
2022-11-04 00:24:12 -0300

    CSS cv button

--- my-profile-app/src/App.tsx
+++ my-profile-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BsSun, BsFillMoonFill } from "react-icons/bs";
 import NavBtnsEn from "./Components/NavBtnsEn.tsx";
 import NavBtnsEs from "./Components/NavBtnsEs.tsx";
 import SocialBanner from "./Components/SocialBanner.tsx";
+import CvButton from "./Components/CvButton.tsx";
 import "./App.css";
 
 const App = ()=> {
@@ -26,7 +27,7 @@ const App = ()=> {
       </div>
       {(en)? (<button id="esp" className="languageBtn" onClick={handleLanguage}></button>): (<button id="usa" className="languageBtn" onClick={handleLanguage}></button>)}
       {(nightMode)? (<BsSun className="modeBtn" id="sun" onClick={handleNightMode}/>): (<BsFillMoonFill className="modeBtn" id="moon" onClick={handleNightMode}/>)}
-      {(en)? (<a href="https://docs.google.com/document/d/1F0UtSyx9vOkeEvWuHf6dbwBOLM5Jfn50WKC3olQnKt8/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK">CV</a>): (<a href="https://docs.google.com/document/d/1MMh8bFdigB-1D7YxAqYZy8mxbGZDxIjxl97UBoeBMdg/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK">CV</a>)}
+      {(en)? (<a href="https://docs.google.com/document/d/1F0UtSyx9vOkeEvWuHf6dbwBOLM5Jfn50WKC3olQnKt8/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK"><CvButton nightMode={nightMode}/></a>): (<a href="https://docs.google.com/document/d/1MMh8bFdigB-1D7YxAqYZy8mxbGZDxIjxl97UBoeBMdg/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK"><CvButton nightMode={nightMode}/></a>)}
     </div>
   )
 }
