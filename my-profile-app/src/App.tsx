5d99f96960e6f31e7acbb3183055f5027e4c4f5d
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-05-26 21:17:23 -0300

    fix download cv

--- my-profile-app/src/App.tsx
+++ my-profile-app/src/App.tsx
@@ -20,6 +20,20 @@ const App = ()=> {
     setEn(!en)
   }
 
+  const handleDownload = ()=> {
+    const link = document.createElement('a');
+    link.href = '../Public/Assets/CV-Márquez-Estefanía.docx';
+    link.download = 'CV-Márquez-Estefanía.docx';
+    link.click();
+  }
+
+  const handleDownloadEn = ()=> {
+    const link = document.createElement('a');
+    link.href = '../Public/Assets/CV(en)-Márquez-Estefanía.docx';
+    link.download = 'CV-Estefanía-Márquez.docx';
+    link.click();
+  }
+
   return (
     <div id="portfolioContainer" className={(nightMode)? "nightMode" : "dayMode"}>
       <SocialBanner nightMode={nightMode}/>
@@ -28,8 +42,8 @@ const App = ()=> {
       </div>
       <button id={en ? "esp" : "usa"} className="languageBtn" onClick={handleLanguage}></button>
       {(nightMode)? (<BsSun className="modeBtn" id="sun" onClick={handleNightMode}/>): (<BsFillMoonFill className="modeBtn" id="moon" onClick={handleNightMode}/>)}
-      {(en)? (<a href="https://docs.google.com/document/d/1F0UtSyx9vOkeEvWuHf6dbwBOLM5Jfn50WKC3olQnKt8/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK"><CvButton nightMode={nightMode}/></a>): (<a href="https://docs.google.com/document/d/1MMh8bFdigB-1D7YxAqYZy8mxbGZDxIjxl97UBoeBMdg/edit?usp=sharing" download="CV Estefanía Márquez" target="_BLANK"><CvButton nightMode={nightMode}/></a>)}
-      <img src={logo} style={{filter: "brightness(1.1)", mixBlendMode: "multiply"}}/>
+      {(en)? (<div onClick={handleDownloadEn}><CvButton nightMode={nightMode}/></div>): (<div onClick={handleDownload}><CvButton nightMode={nightMode}/></div>)}
+      {/* <img src={logo} style={{filter: "brightness(1.1)", mixBlendMode: "multiply"}}/> */}
       <ContactForm en={en} />
       
     </div>
