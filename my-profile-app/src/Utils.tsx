b25e0672f6cc302b07266cc98bd36ec6399f56b5
MarquezE92 <vsuykyasmbp@hotmail.com>
2024-02-15 22:44:23 -0300

    fix img reference

--- my-profile-app/src/Utils.tsx
+++ my-profile-app/src/Utils.tsx
@@ -3,85 +3,85 @@ export const projects = [
     descriptionEs: "Web app que da a los profesionales y establecimientos que se registran una plataforma desde donde llevar el control de su actividad, más una tienda online para que sus clientes puedan reservar turnos.", 
     descriptionEn: "Web that gives registered professionals and sestablishments a platform from which to administrate their activity, plus an online store so that their clients can book appointments.", 
     technologies: "React, Redux, CSS, MUI, SDK Mercadopago, React Google Charts, Node.js, Express, Cloudinary, Bcrypt, JSON Web Token, OAuth 2.0, Nodemailer, API Mercadopago, Ultramsg, Mobbex, MongoDB, Mongoose, AWS, Nginx, Hostinger.",
-    page: "", repo: "", imgs: ["../public/assets/images/Inicio.png", "../public/assets/images/AgregarSuc.png", "../public/assets/images/VistaTurnosDesk.png", "../public/assets/images/Métricas.png", "../public/assets/images/Planes.png", "../public/assets/images/Extras.png", "../public/assets/images/PacienteMob1.png", "../public/assets/images/PacienteMob2.png", "../public/assets/images/CapturaTienda.png", "../public/assets/images/CapturaAgendando.png"]}, 
+    page: "", repo: "", imgs: ["../assets/images/Inicio.png", "../assets/images/AgregarSuc.png", "../assets/images/VistaTurnosDesk.png", "../assets/images/Métricas.png", "../assets/images/Planes.png", "../assets/images/Extras.png", "../assets/images/PacienteMob1.png", "../assets/images/PacienteMob2.png", "../assets/images/CapturaTienda.png", "../assets/images/CapturaAgendando.png"]}, 
     {title: "SnorInn-App", 
     descriptionEs: "Web app que conecta a aquellos con un espacio disponible para hospedar personas, y aquellas personas que quieren encontrar un lugar para descansar.", 
     descriptionEn: "Web app that connects those with a space available to host people, and those who want to find a place to rest.",
     technologies: "React, Redux Toolkit, TypeScript, CSS, Bootstrap, Node.js, Express, Cloudinary, Bcrypt, JSON Web Token, Nodemailer, Stripe, Google-auth-library, MongoDB, Mongoose, Vercel, Trello, Scrum.", 
-    page: "https://snor-inn.vercel.app/", repo: "https://github.com/MarquezE92/SnorInn-App", imgs: ["../public/assets/images/SnorInn.png", "../public/assets/images/habitaciones.png", "../public/assets/images/detalleRoom.png", "../public/assets/images/review.png"]}, 
+    page: "https://snor-inn.vercel.app/", repo: "https://github.com/MarquezE92/SnorInn-App", imgs: ["../assets/images/SnorInn.png", "../assets/images/habitaciones.png", "../assets/images/detalleRoom.png", "../assets/images/review.png"]}, 
     {title: "Pokemon-App", 
     descriptionEs: "Web App que te permite explorar los 40 primeros pokémons y crear los tuyos.", 
     descriptionEn: "Web App that allows you to explore the first 40 pokémons and create your own.",
     technologies: "React, Redux, vanilla CSS, Node.js, Express, PostgreSQL, Sequelize, Vercel, Railway.", 
-    page: "", repo: "https://github.com/MarquezE92/PI-Pokemon",  imgs: ["../public/assets/images/Landing.png", "../public/assets/images/home.png", "../public/assets/images/form.png", "../public/assets/images/cards.png", "../public/assets/images/Detail.png",]}]
+    page: "", repo: "https://github.com/MarquezE92/PI-Pokemon",  imgs: ["../assets/images/Landing.png", "../assets/images/home.png", "../assets/images/form.png", "../assets/images/cards.png", "../assets/images/Detail.png",]}]
 
 export const technologies = [
     {name: "HTML",
-    img: "../public/assets/images/html_icon.svg"
+    img: "../assets/images/html_icon.svg"
 },
 {name: "CSS",
-    img: "../public/assets/images/css_icon.svg"
+    img: "../assets/images/css_icon.svg"
 },
 {name: "JavaScript",
-    img: "../public/assets/images/javascript_icon.svg"
+    img: "../assets/images/javascript_icon.svg"
 },
 {name: "TypeScript",
-    img: "../public/assets/images/typescript_icon.svg"
+    img: "../assets/images/typescript_icon.svg"
 },
 {name: "React",
-    img: "../public/assets/images/react_icon.svg"
+    img: "../assets/images/react_icon.svg"
 },
 {name: "Redux",
-    img: "../public/assets/images/redux_icon.svg"
+    img: "../assets/images/redux_icon.svg"
 },
 {name: "Node.js",
-    img: "../public/assets/images/node_icon.svg"
+    img: "../assets/images/node_icon.svg"
 },
 {name: "Express",
-    img: "../public/assets/images/expressjs_icon.svg"
+    img: "../assets/images/expressjs_icon.svg"
 },
 {name: "Python",
-    img: "../public/assets/images/python_icon.svg"
+    img: "../assets/images/python_icon.svg"
 },
 {
     name: "PostgreSQL",
-    img: "../public/assets/images/postgresql_icon.svg"
+    img: "../assets/images/postgresql_icon.svg"
 },
 {
     name: "Sequelize",
-    img: "../public/assets/images/sequelize_icon.svg"
+    img: "../assets/images/sequelize_icon.svg"
 },
 {
     name: "MongoDB",
-    img: "../public/assets/images/mongodb_icon.svg"
+    img: "../assets/images/mongodb_icon.svg"
 },
 {
     name: "Bootstrap",
-    img: "../public/assets/images/bootstrap_icon.svg"
+    img: "../assets/images/bootstrap_icon.svg"
 },
 {
     name: "MUI",
-    img: "../public/assets/images/mui_icon.svg"
+    img: "../assets/images/mui_icon.svg"
 },
 
 {
     name: "Scrum",
-    img: "../public/assets/images/scrum_icon.svg"
+    img: "../assets/images/scrum_icon.svg"
 },
 {
     name: "Git",
-    img: "../public/assets/images/git_icon.svg"
+    img: "../assets/images/git_icon.svg"
 },
 {
     name: "GitHub",
-    img: "../public/assets/images/github_icon.svg"
+    img: "../assets/images/github_icon.svg"
 },
 {
     name: "Node package manager",
-    img: "../public/assets/images/npm_icon.svg"
+    img: "../assets/images/npm_icon.svg"
 },
 {
     name: "Postman",
-    img: "../public/assets/images/postman.png"
+    img: "../assets/images/postman.png"
 }
 ]
